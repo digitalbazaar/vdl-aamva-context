f708a770ef81dc6228780c191e595214c28cdd3e
Kayode Ezike <kezike13@gmail.com>
2023-11-06 18:25:31 -0500

    Move constants to their own file.

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+module.exports = {
+  // default context output filename under ./contexts/
+  CONTEXT_FILENAME: 'vdl-aamva-v1.jsonld',
+  // default context well known URL id
+  CONTEXT_URL: 'https://w3id.org/vdl/aamva/v1',
+  // default context CBOR-LD codec id
+  // value between 0x0 and 0x7FFF for globally registered term codec values
+  // or >= 0x8000 for app-specific local terms
+  CBORLD_VALUE: 0x0
+};
--- js/index.js
+++ js/index.js
@@ -4,29 +4,20 @@
 'use strict';
 
 const context = require('./context');
-
-const constants = {
-  // default context output filename under ./contexts/
-  CONTEXT_FILENAME: 'vdl-aamva-v1.jsonld',
-  // default context well known URL id
-  CONTEXT_URL: 'https://w3id.org/vdl/aamva/v1',
-  // default context CBOR-LD codec id
-  // value between 0x0 and 0x7FFF for globally registered term codec values
-  // or >= 0x8000 for app-specific local terms
-  CBORLD_VALUE: 0x0
-};
+const constants = require('./constants');
+const {CONTEXT_URL, CBORLD_VALUE} = constants;
 
 // map of all context URLs to context data
 const contexts = new Map();
-contexts.set(constants.CONTEXT_URL, context);
+contexts.set(CONTEXT_URL, context);
 
 // map of all context URLs to CBOR-LD codec ids
 const appContextMap = new Map();
-appContextMap.set(constants.CONTEXT_URL, constants.CBORLD_VALUE);
+appContextMap.set(CONTEXT_URL, CBORLD_VALUE);
 
 module.exports = {
   CONTEXT: context,
-  CONTEXT_URL: constants.CONTEXT_URL,
+  CONTEXT_URL: CONTEXT_URL,
   appContextMap,
   constants,
   contexts
