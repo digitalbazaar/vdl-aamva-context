9d42105b4278d087f8e35ddc1e86ef42eb75a3fb
Kayode Ezike <kezike13@gmail.com>
2023-12-19 16:21:59 -0500

    Remove duplicate key in .eslintrc.cjs.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,12 @@
 module.exports = {
   root: true,
   extends: [
-    'eslint-config-digitalbazaar'
+    'digitalbazaar',
+    'digitalbazaar/module'
   ],
   env: {
     node: true
   },
-  extends: [
-    'digitalbazaar',
-    'digitalbazaar/module'
-  ],
   rules: {
     'unicorn/prefer-node-protocol': 'error'
   },
