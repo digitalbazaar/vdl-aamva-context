7aa3a709e0012153cae20e22aab192c515271c68
Kayode Ezike <kezike13@gmail.com>
2023-12-19 20:11:55 -0500

    Import constants from separate file.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
+import constants from './constants';
 import context from './context.js';
-
-export const constants = {
-  // default context output filename under ./contexts/
-  CONTEXT_FILENAME: 'vdl-aamva-v1.jsonld',
-  // default context well known URL id
-  CONTEXT_URL: 'https://w3id.org/vdl/aamva/v1',
-  // default context CBOR-LD codec id
-  // value between 0x0 and 0x7FFF for globally registered term codec values
-  // or >= 0x8000 for app-specific local terms
-  CBORLD_VALUE: 0x0
-};
 
 // map of all context URLs to context data
 export const contexts = new Map();
